fa58c35cd8025376786ed9c4f773a07cf526d2fb
Tatu Peltola <tatu.peltola@gmail.com>
2023-01-17 01:42:01 +0200

    Implement SPI transfers

--- SoapySX/SoapySX.cpp
+++ SoapySX/SoapySX.cpp
@@ -2,21 +2,94 @@
 #include <SoapySDR/Registry.hpp>
 #include <SoapySDR/Logger.hpp>
 
+#include <string.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
 /***********************************************************************
  * Device interface
  **********************************************************************/
 class SoapySX : public SoapySDR::Device
 {
+private:
+    int spi;
+
 public:
-    SoapySX(const SoapySDR::Kwargs &args)
+    SoapySX(const SoapySDR::Kwargs &args):
+        spi(-1)
     {
         (void)args;
         SoapySDR_logf(SOAPY_SDR_INFO, "Initializing SoapySX");
+
+        // TODO: support custom SPIDEV path as an argument
+        spi = open("/dev/spidev0.0", O_RDWR);
+        SoapySDR_logf(SOAPY_SDR_DEBUG, "SPIDEV opened: %d", spi);
     }
 
     ~SoapySX(void)
     {
         SoapySDR_logf(SOAPY_SDR_INFO, "Uninitializing SoapySX");
+
+        if (spi >= 0)
+            close(spi);
+    }
+
+private:
+    // The transactSPI API in SoapySDR is a bit weird, using only
+    // a single unsigned int for the data.
+    // Here is a function that also allows longer transfers.
+    int spi_transfer(const uint8_t *tx_data, uint8_t *rx_data, const size_t numBytes)
+    {
+        if (spi < 0)
+            return -1;
+
+        struct spi_ioc_transfer transfer;
+        memset(&transfer, 0, sizeof(transfer));
+
+        transfer.tx_buf = (__u64)tx_data;
+        transfer.rx_buf = (__u64)rx_data;
+        transfer.len = (__u32)numBytes;
+
+        int ret = ioctl(spi, SPI_IOC_MESSAGE(1), &transfer);
+        SoapySDR_logf(SOAPY_SDR_DEBUG, "SPIDEV ioctl: %d", ret);
+
+        return ret;
+    }
+
+public:
+    // Wrap spi_transfer so that short raw SPI transfers
+    // are possible through the SoapySDR API.
+    unsigned transactSPI(const int addr, const unsigned data, const size_t numBits)
+    {
+        // Use address 0 for the radio chip. Other addresses are unused.
+        if (addr != 0) {
+            SoapySDR_logf(SOAPY_SDR_ERROR, "Invalid SPI address %d", addr);
+            return 0;
+        }
+        // Only whole bytes are supported. The bits should also fit in unsigned.
+        if (((numBits % 8) != 0) || (numBits > 8*sizeof(unsigned))) {
+            SoapySDR_logf(SOAPY_SDR_ERROR, "Invalid SPI transfer length %d", numBits);
+            return 0;
+        }
+        const size_t numBytes = numBits / 8;
+
+        uint8_t buf[sizeof(unsigned)];
+        // Put most significant bits first
+        for (size_t i = 0; i < numBytes; i++)
+            buf[i] = data >> (numBits - 8 * (i+1));
+
+        if (spi_transfer(buf, buf, numBytes) < 0)
+            return 0;
+
+        // Pack the result to unsigned, MSB first
+        unsigned received = 0;
+        for (size_t i = 0; i < numBytes; i++)
+            received = (received << 8) | (unsigned)buf[i];
+        return received;
     }
 };
 
--- SoapySX/test.py
+++ SoapySX/test.py
@@ -4,9 +4,15 @@
 import SoapySDR
 
 def main():
+    SoapySDR.setLogLevel(SoapySDR.SOAPY_SDR_DEBUG)
+
     device = SoapySDR.Device({
         'driver': 'sx',
     })
+
+    # Test reading chip version number
+    # using the raw SPI transfer API.
+    print("%04x" % device.transactSPI(0, 0x0700, 16))
 
 if __name__ == '__main__':
     main()
