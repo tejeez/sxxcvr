623c03022d22230e6d10f75d534585af84d64f4d
Tatu Peltola <tatu.peltola@gmail.com>
2024-03-03 19:08:25 +0200

    Remove obsolete test scripts and options

--- SoapySX/test_i2s_rx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: MIT
-#
-# Script for testing I2S before the ALSA interface is implemented in SoapySX.
-arecord -D "hw:CARD=SX1255,DEV=1" -r 96000 -c 2 -f S32_LE -t raw rx_test.raw
--- SoapySX/test_i2s_tx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: MIT
-#
-# Script for testing I2S before the ALSA interface is implemented in SoapySX.
-# Transmit some random noise.
-aplay -D "hw:CARD=SX1255,DEV=0" -r 96000 -c 2 -f S32_LE -t raw /dev/urandom
